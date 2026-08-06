aada61a04d244d3cf3f1ec93fce8ac5e60502c90
Johan <johandhaneja2568@gmail.com>
2026-08-06 17:40:41 +0530

    Time: 0 ms (100%), Space: 18.5 MB (43.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,39 +1,41 @@
 class Solution {
 private:
-    bool dfs(int node,vector<vector<int>>& adj,vector<int>& vis,vector<int>& pathvis,stack<int>& st){
-        vis[node] = 1;
+    bool dfs(vector<vector<int>>& adj,vector<int>& pathvis,vector<int>& visited,int node,stack<int>& stk){
+        visited[node] = 1;
         pathvis[node] = 1;
-        for(auto neigh:adj[node]){
-            if(!vis[neigh]){
-                if(dfs(neigh,adj,vis,pathvis,st)) return true;;
-            }else if(vis[neigh] == 1 && pathvis[neigh] == 1){
+        for(auto& neigh:adj[node]){
+            if(!visited[neigh]){
+                if (dfs(adj,pathvis,visited,neigh,stk)) return true;
+            }else if(pathvis[neigh]){
                 return true;
             }
         }
         pathvis[node] = 0;
-        st.push(node);
+        stk.push(node);
         return false;
     }
 public:
-    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(V);
-        for(auto edge:prerequisites){
-            adj[edge[1]].push_back(edge[0]);
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> adj(n);
+        for(auto& edge:prerequisites){
+            int u = edge[0],v = edge[1];
+            adj[v].push_back(u);
         }
-
-        vector<int> vis(V,0);
-        vector<int> pathvis(V,0);
-        stack<int> st;
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,pathvis,st)) return {};
+        vector<int> visited(n,0);
+        vector<int> pathvis(n,0);
+        stack<int> stk;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(dfs(adj,pathvis,visited,i,stk)) return {};
             }
         }
-        vector<int> topo;
-        while(!st.empty()){
-            topo.push_back(st.top());
-            st.pop();
+        vector<int> ans;
+        while(!stk.empty()){
+            ans.push_back(stk.top());
+            stk.pop();
         }
-        return topo;
+        return ans;
+
     }
 };
