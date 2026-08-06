f6feeb0e22aedcf470b271f3f2e894c8da72d78e
Johan <johandhaneja2568@gmail.com>
2026-08-06 17:12:32 +0530

    Time: 3 ms (75.2%), Space: 19.3 MB (54.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,32 @@
 class Solution {
+private:
+    bool dfs(vector<vector<int>>& adj,vector<int>& visited,int node,vector<int>& pathvis){
+        visited[node] = 1;
+        pathvis[node] = 1;
+        for(auto& neigh:adj[node]){
+            if(!visited[neigh]){
+                if(dfs(adj,visited,neigh,pathvis)) return true;
+            }else if(pathvis[neigh] == 1) return true;
+        }
+        pathvis[node] = 0;
+        return false;
+    }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V = numCourses;
-
-        vector<vector<int>> adj(V);
+        int n = numCourses;
+        vector<vector<int>> adj(n);
         for(auto& edge:prerequisites){
-            int u = edge[0];
-            int v = edge[1];
+            int u = edge[0],v = edge[1];
             adj[u].push_back(v);
         }
-        vector<int> indegree(V,0);
-        for(int i=0;i<V;i++){
-            for(auto neigh:adj[i]){
-                indegree[neigh]++;
+        vector<int> visited(n,0);
+        vector<int> pathvis(n,0);
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if (dfs(adj,visited,i,pathvis)) return false;
             }
         }
-        queue<int> q;
-        for(int i=0;i<V;i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
-        }
-        vector<int> topo;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto neigh:adj[node]){
-                indegree[neigh]--;
-                if(indegree[neigh] == 0) q.push(neigh);
-            }
-        }
-        if(topo.size() == V) return true;
-        return false;
+        return true;
     }
 };
